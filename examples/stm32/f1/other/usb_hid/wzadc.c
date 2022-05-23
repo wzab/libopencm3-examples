e54a3aace06a7b3274371c97c74379c63feb5c8f
Wojciech M. Zabołotny <wzab01@gmail.com>
2022-05-24 00:45:12 +0200

    Version that at least enumerates

--- examples/stm32/f1/other/usb_hid/wzadc.c
+++ examples/stm32/f1/other/usb_hid/wzadc.c
@@ -1,6 +1,6 @@
 /*
- * This file is part of the libopencm3 project.
- *
+ * Simple ADC based on STM32.
+ * Based on work of Gareth McMullin
  * Copyright (C) 2010 Gareth McMullin <gareth@blacksphere.co.nz>
  *
  * This library is free software: you can redistribute it and/or modify
@@ -26,7 +26,7 @@
 #include <libopencm3/usb/hid.h>
 
 /* Define this to include the DFU APP interface. */
-#define INCLUDE_DFU_INTERFACE
+//#define INCLUDE_DFU_INTERFACE
 
 #ifdef INCLUDE_DFU_INTERFACE
 #include <libopencm3/cm3/scb.h>
@@ -35,7 +35,7 @@
 
 static usbd_device *usbd_dev;
 
-const struct usb_devicx	e_descriptor dev_descr = {
+const struct usb_device_descriptor dev_descr = {
 	.bLength = USB_DT_DEVICE_SIZE,
 	.bDescriptorType = USB_DT_DEVICE,
 	.bcdUSB = 0x0200,
@@ -52,91 +52,34 @@ const struct usb_devicx	e_descriptor dev_descr = {
 	.bNumConfigurations = 1,
 };
 
-static const uint8_t hid_report_descriptor[] = {
-	0x05, 0x01, /* USAGE_PAGE (Generic Desktop)         */
-	0x09, 0x02, /* USAGE (Mouse)                        */
-	0xa1, 0x01, /* COLLECTION (Application)             */
-	0x09, 0x01, /*   USAGE (Pointer)                    */
-	0xa1, 0x00, /*   COLLECTION (Physical)              */
-	0x05, 0x09, /*     USAGE_PAGE (Button)              */
-	0x19, 0x01, /*     USAGE_MINIMUM (Button 1)         */
-	0x29, 0x03, /*     USAGE_MAXIMUM (Button 3)         */
-	0x15, 0x00, /*     LOGICAL_MINIMUM (0)              */
-	0x25, 0x01, /*     LOGICAL_MAXIMUM (1)              */
-	0x95, 0x03, /*     REPORT_COUNT (3)                 */
-	0x75, 0x01, /*     REPORT_SIZE (1)                  */
-	0x81, 0x02, /*     INPUT (Data,Var,Abs)             */
-	0x95, 0x01, /*     REPORT_COUNT (1)                 */
-	0x75, 0x05, /*     REPORT_SIZE (5)                  */
-	0x81, 0x01, /*     INPUT (Cnst,Ary,Abs)             */
-	0x05, 0x01, /*     USAGE_PAGE (Generic Desktop)     */
-	0x09, 0x30, /*     USAGE (X)                        */
-	0x09, 0x31, /*     USAGE (Y)                        */
-	0x09, 0x38, /*     USAGE (Wheel)                    */
-	0x15, 0x81, /*     LOGICAL_MINIMUM (-127)           */
-	0x25, 0x7f, /*     LOGICAL_MAXIMUM (127)            */
-	0x75, 0x08, /*     REPORT_SIZE (8)                  */
-	0x95, 0x03, /*     REPORT_COUNT (3)                 */
-	0x81, 0x06, /*     INPUT (Data,Var,Rel)             */
-	0xc0,       /*   END_COLLECTION                     */
-	0x09, 0x3c, /*   USAGE (Motion Wakeup)              */
-	0x05, 0xff, /*   USAGE_PAGE (Vendor Defined Page 1) */
-	0x09, 0x01, /*   USAGE (Vendor Usage 1)             */
-	0x15, 0x00, /*   LOGICAL_MINIMUM (0)                */
-	0x25, 0x01, /*   LOGICAL_MAXIMUM (1)                */
-	0x75, 0x01, /*   REPORT_SIZE (1)                    */
-	0x95, 0x02, /*   REPORT_COUNT (2)                   */
-	0xb1, 0x22, /*   FEATURE (Data,Var,Abs,NPrf)        */
-	0x75, 0x06, /*   REPORT_SIZE (6)                    */
-	0x95, 0x01, /*   REPORT_COUNT (1)                   */
-	0xb1, 0x01, /*   FEATURE (Cnst,Ary,Abs)             */
-	0xc0        /* END_COLLECTION                       */
-};
-
-static const struct {
-	struct usb_hid_descriptor hid_descriptor;
-	struct {
-		uint8_t bReportDescriptorType;
-		uint16_t wDescriptorLength;
-	} __attribute__((packed)) hid_report;
-} __attribute__((packed)) hid_function = {
-	.hid_descriptor = {
-		.bLength = sizeof(hid_function),
-		.bDescriptorType = USB_DT_HID,
-		.bcdHID = 0x0100,
-		.bCountryCode = 0,
-		.bNumDescriptors = 1,
-	},
-	.hid_report = {
-		.bReportDescriptorType = USB_DT_REPORT,
-		.wDescriptorLength = sizeof(hid_report_descriptor),
-	}
-};
-
-const struct usb_endpoint_descriptor hid_endpoint = {
+static const struct usb_endpoint_descriptor adc_endp[] = {{
 	.bLength = USB_DT_ENDPOINT_SIZE,
 	.bDescriptorType = USB_DT_ENDPOINT,
-	.bEndpointAddress = 0x81,
-	.bmAttributes = USB_ENDPOINT_ATTR_INTERRUPT,
-	.wMaxPacketSize = 4,
-	.bInterval = 0x20,
-};
+	.bEndpointAddress = 0x01,
+	.bmAttributes = USB_ENDPOINT_ATTR_BULK,
+	.wMaxPacketSize = 64,
+},{
 
-const struct usb_interface_descriptor hid_iface = {
+	.bLength = USB_DT_ENDPOINT_SIZE,
+	.bDescriptorType = USB_DT_ENDPOINT,
+	.bEndpointAddress = 0x82,
+	.bmAttributes = USB_ENDPOINT_ATTR_BULK,
+	.wMaxPacketSize = 64,
+}};
+
+const struct usb_interface_descriptor adc_iface = {
 	.bLength = USB_DT_INTERFACE_SIZE,
 	.bDescriptorType = USB_DT_INTERFACE,
 	.bInterfaceNumber = 0,
 	.bAlternateSetting = 0,
-	.bNumEndpoints = 1,
-	.bInterfaceClass = USB_CLASS_HID,
-	.bInterfaceSubClass = 1, /* boot */
-	.bInterfaceProtocol = 2, /* mouse */
+	.bNumEndpoints = 2,
+	.bInterfaceClass = USB_CLASS_VENDOR,
+	.bInterfaceSubClass = 0xff,
+	.bInterfaceProtocol = 0xff,
 	.iInterface = 0,
 
-	.endpoint = &hid_endpoint,
+	.endpoint = adc_endp,
 
-	.extra = &hid_function,
-	.extralen = sizeof(hid_function),
 };
 
 #ifdef INCLUDE_DFU_INTERFACE
@@ -154,7 +97,7 @@ const struct usb_interface_descriptor dfu_iface = {
 	.bDescriptorType = USB_DT_INTERFACE,
 	.bInterfaceNumber = 1,
 	.bAlternateSetting = 0,
-	.bNumEndpoints = 0,
+	.bNumEndpoints = 1,
 	.bInterfaceClass = 0xFE,
 	.bInterfaceSubClass = 1,
 	.bInterfaceProtocol = 1,
@@ -167,7 +110,7 @@ const struct usb_interface_descriptor dfu_iface = {
 
 const struct usb_interface ifaces[] = {{
 	.num_altsetting = 1,
-	.altsetting = &hid_iface,
+	.altsetting = &adc_iface,
 #ifdef INCLUDE_DFU_INTERFACE
 }, {
 	.num_altsetting = 1,
@@ -201,24 +144,6 @@ static const char *usb_strings[] = {
 /* Buffer to be used for control requests. */
 uint8_t usbd_control_buffer[128];
 
-static enum usbd_request_return_codes hid_control_request(usbd_device *dev, struct usb_setup_data *req, uint8_t **buf, uint16_t *len,
-			void (**complete)(usbd_device *, struct usb_setup_data *))
-{
-	(void)complete;
-	(void)dev;
-
-	if((req->bmRequestType != 0x81) ||
-	   (req->bRequest != USB_REQ_GET_DESCRIPTOR) ||
-	   (req->wValue != 0x2200))
-		return USBD_REQ_NOTSUPP;
-
-	/* Handle the HID report descriptor. */
-	*buf = (uint8_t *)hid_report_descriptor;
-	*len = sizeof(hid_report_descriptor);
-
-	return USBD_REQ_HANDLED;
-}
-
 #ifdef INCLUDE_DFU_INTERFACE
 static void dfu_detach_complete(usbd_device *dev, struct usb_setup_data *req)
 {
@@ -247,18 +172,44 @@ static enum usbd_request_return_codes dfu_control_request(usbd_device *dev, stru
 }
 #endif
 
-static void hid_set_config(usbd_device *dev, uint16_t wValue)
+static enum usbd_request_return_codes adc_control_request(usbd_device *dev, struct usb_setup_data *req, uint8_t **buf, uint16_t *len,
+			void (**complete)(usbd_device *, struct usb_setup_data *))
+{
+	(void)complete;
+	(void)dev;
+
+	if((req->bmRequestType != 0x81) ||
+	   (req->bRequest != USB_REQ_GET_DESCRIPTOR) ||
+	   (req->wValue != 0x2200))
+		return USBD_REQ_NOTSUPP;
+
+	return USBD_REQ_HANDLED;
+}
+
+static void adccfg_data_rx_cb(usbd_device *usbd_dev, uint8_t ep)
+{
+	(void)ep;
+
+	char buf[64];
+	char msg[] = "Odebralem:";
+	int len = usbd_ep_read_packet(usbd_dev, 0x01, buf, 64);
+	if(len) {
+	  usbd_ep_write_packet(usbd_dev,0x82,msg,sizeof(msg));
+	  usbd_ep_write_packet(usbd_dev,0x82,buf,len);
+	  }
+}
+static void adc_set_config(usbd_device *dev, uint16_t wValue)
 {
 	(void)wValue;
 	(void)dev;
 
-	usbd_ep_setup(dev, 0x81, USB_ENDPOINT_ATTR_INTERRUPT, 4, NULL);
-
-	usbd_register_control_callback(
+	usbd_ep_setup(dev, 0x01, USB_ENDPOINT_ATTR_BULK, 64, adccfg_data_rx_cb);
+	usbd_ep_setup(dev, 0x82, USB_ENDPOINT_ATTR_BULK, 64, NULL);
+	/*usbd_register_control_callback(
 				dev,
 				USB_REQ_TYPE_STANDARD | USB_REQ_TYPE_INTERFACE,
 				USB_REQ_TYPE_TYPE | USB_REQ_TYPE_RECIPIENT,
-				hid_control_request);
+				adc_control_request);*/
 #ifdef INCLUDE_DFU_INTERFACE
 	usbd_register_control_callback(
 				dev,
@@ -296,7 +247,7 @@ int main(void)
 	}
 
 	usbd_dev = usbd_init(&st_usbfs_v1_usb_driver, &dev_descr, &config, usb_strings, 3, usbd_control_buffer, sizeof(usbd_control_buffer));
-	usbd_register_set_config_callback(usbd_dev, hid_set_config);
+	usbd_register_set_config_callback(usbd_dev, adc_set_config);
 
 	while (1)
 		usbd_poll(usbd_dev);
@@ -315,5 +266,5 @@ void sys_tick_handler(void)
 	if (x < -30)
 		dir = -dir;
 
-	usbd_ep_write_packet(usbd_dev, 0x81, buf, 4);
+//	usbd_ep_write_packet(usbd_dev, 0x81, buf, 4);
 }
